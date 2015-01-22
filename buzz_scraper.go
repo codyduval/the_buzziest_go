b83782afd95ef2b3d43d2ec4d44940f279a90369
Cody Duval <cody.duval@gmail.com>
2015-01-22 14:49:38 -0500

    Adding goroutine

--- buzz_scraper.go
+++ buzz_scraper.go
@@ -23,31 +23,58 @@ type (
 
 func main() {
 
-	url := "http://blog.codyduval.com"
-	// Xpath to the title of the first blog post
-	node := "/html/body/div/div/div/div/div[1]/div[1]/a/h3"
+	var url string
+	var xpath string
+	pages_to_pull := 5
 
+	for page := 1; page <= pages_to_pull; page++ {
+		url = fmt.Sprintf("http://www.tastingtable.com/dispatch/national/dispatch?applyFilter=true&filterEditionId=1&filterNeighborhoodId=0&filterDropDown1=opened&requestedPage=%v", page)
+
+		response := make(chan *http.Response)
+		go fetchPage(url, response)
+		root := parsePage(response)
+
+		for node := 2; node <= 6; node++ {
+			xpath = fmt.Sprintf("//*[@id='dispatch']/div[1]/div[6]/div[4]/div[%v]/div/h1", node)
+
+			name := scrapePage(root, xpath)
+			fmt.Println("Found:", name)
+
+		}
+	}
+
+}
+
+func fetchPage(url string, response chan *http.Response) {
 	r, err := http.Get(url)
 	if err != nil {
 		fmt.Printf("%s", err)
 	}
+	response <- r
+}
 
-	path, err := xmlpath.Compile(node)
-	if err != nil {
-		fmt.Println("Didn't work!")
-		log.Fatal(err)
-	}
+func parsePage(r chan *http.Response) *xmlpath.Node {
 
 	root, err := xmlpath.ParseHTML(r.Body)
 	if err != nil {
 		fmt.Println("Bummer, I couldn't parse that blob of html!")
 		log.Fatal(err)
 	}
+	return root
 
-	if value, ok := path.String(root); ok {
-		fmt.Println("Found:", value)
+}
+
+func scrapePage(root *xmlpath.Node, xpath string) string {
+	path, err := xmlpath.Compile(xpath)
+	if err != nil {
+		fmt.Println("Didn't work!")
+		log.Fatal(err)
+	}
+
+	if name, ok := path.String(root); ok {
+		return name
 	} else {
-		fmt.Println("Sorry, couldn't find anything at the xpath node: " + node)
+		return "Sorry, couldn't find anything at the xpath node"
 	}
 
 }
